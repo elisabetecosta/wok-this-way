24e5cd41a37df39c6e2af00284cb0cc3db8d3f3e
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-11-20 19:24:25 +0000

    add loading state

--- app/(root)/buffets/page.tsx
+++ app/(root)/buffets/page.tsx
@@ -1,25 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
+
+import { Buffet } from "@/types";
 
 import { fetchBuffets } from "@/lib/actions/buffet.actions";
 
 
-export default async function BuffetsPage() {
+export default function BuffetsPage() {
 
-  const buffets = await fetchBuffets();
+  const [buffets, setBuffets] = useState<Buffet[] | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+
+    async function getBuffets() {
+
+      const buffets = await fetchBuffets();
+      setBuffets(buffets);
+      setLoading(false);
+    };
+
+    getBuffets();
+
+  }, []);
+
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
 
   return (
     <main className="p-4">
-       <h1>All Buffets</h1> 
+      <h1>All Buffets</h1>
       <div className="mb-10">
-        {buffets.length ? (
-            buffets.map((buffet) => (
-                <div key={buffet._id}>
-                  <Link href={`/buffets/${buffet._id}`}>
-                  <h2 className="text-3xl">{buffet.name}</h2>
-                  </Link>
-                </div>
-              ))
+        {buffets && buffets.length ? (
+          buffets.map((buffet) => (
+            <div key={buffet._id}>
+              <Link href={`/buffets/${buffet._id}`}>
+                <h2 className="text-3xl">{buffet.name}</h2>
+              </Link>
+            </div>
+          ))
         ) : <h2>No buffets found</h2>}
       </div>
     </main>
